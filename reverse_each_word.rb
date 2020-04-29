5aa58a2cddec5182554a0cb8a6fc72c9b6050e50
Felix Rodriguez <frod25@gmail.com>
2020-04-29 22:40:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def reverse_each_word(sentence)
-  reversed = sentence.split(/ /).map { |word| word.reverse }
+  reversed = sentence.split(/ /).map { |word| word.reverse }.join(" ")
 end
 
 binding.pry
